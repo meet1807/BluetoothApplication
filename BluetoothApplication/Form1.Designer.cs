e349897b1f263998040cbdb1211d3eee6fa63743
Meet Patel <mpatel.patel180796@gmail.com>
2016-09-20 04:19:01 +0530

    v1

---
+++ BluetoothApplication/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace BluetoothApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox_comport = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ComboBox_baud = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DataViewer = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFolderPath = new System.Windows.Forms.TextBox();
+            this.buttonFolderBrowser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox_comport
+            // 
+            this.comboBox_comport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_comport.FormattingEnabled = true;
+            this.comboBox_comport.Location = new System.Drawing.Point(108, 105);
+            this.comboBox_comport.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBox_comport.Name = "comboBox_comport";
+            this.comboBox_comport.Size = new System.Drawing.Size(70, 23);
+            this.comboBox_comport.TabIndex = 0;
+            this.comboBox_comport.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM PORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // ComboBox_baud
+            // 
+            this.ComboBox_baud.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox_baud.FormattingEnabled = true;
+            this.ComboBox_baud.Location = new System.Drawing.Point(108, 137);
+            this.ComboBox_baud.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ComboBox_baud.Name = "ComboBox_baud";
+            this.ComboBox_baud.Size = new System.Drawing.Size(85, 23);
+            this.ComboBox_baud.TabIndex = 3;
+            this.ComboBox_baud.SelectedIndexChanged += new System.EventHandler(this.ComboBox_baud_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Magneto", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label3.Location = new System.Drawing.Point(13, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(435, 35);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bluetooth Serial Terminal";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(8, 182);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 29);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop.Location = new System.Drawing.Point(8, 232);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(112, 29);
+            this.buttonStop.TabIndex = 7;
+            this.buttonStop.Text = "STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label4.Location = new System.Drawing.Point(5, 448);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "© InationLabs 2015";
+            this.label4.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // DataViewer
+            // 
+            this.DataViewer.Location = new System.Drawing.Point(201, 91);
+            this.DataViewer.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.DataViewer.Name = "DataViewer";
+            this.DataViewer.Size = new System.Drawing.Size(579, 372);
+            this.DataViewer.TabIndex = 4;
+            this.DataViewer.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(89, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "OutputFolder";
+            // 
+            // textBoxFolderPath
+            // 
+            this.textBoxFolderPath.Location = new System.Drawing.Point(201, 58);
+            this.textBoxFolderPath.Name = "textBoxFolderPath";
+            this.textBoxFolderPath.Size = new System.Drawing.Size(365, 22);
+            this.textBoxFolderPath.TabIndex = 10;
+            this.textBoxFolderPath.TextChanged += new System.EventHandler(this.textBoxFolderPath_TextChanged);
+            // 
+            // buttonFolderBrowser
+            // 
+            this.buttonFolderBrowser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFolderBrowser.Location = new System.Drawing.Point(572, 56);
+            this.buttonFolderBrowser.Name = "buttonFolderBrowser";
+            this.buttonFolderBrowser.Size = new System.Drawing.Size(80, 27);
+            this.buttonFolderBrowser.TabIndex = 11;
+            this.buttonFolderBrowser.Text = "OPEN";
+            this.buttonFolderBrowser.UseVisualStyleBackColor = true;
+            this.buttonFolderBrowser.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(793, 475);
+            this.Controls.Add(this.buttonFolderBrowser);
+            this.Controls.Add(this.textBoxFolderPath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DataViewer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ComboBox_baud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_comport);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Bluetooth Serial Terminal (InationLabs)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_comport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ComboBox_baud;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.RichTextBox DataViewer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFolderPath;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonFolderBrowser;
+    }
+}
+
---
+++ BluetoothApplication/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+
+namespace BluetoothApplication
+{
+    public partial class Form1 : Form
+    {
+        TextWriter tw; 
+        delegate void SetTextCallback(string text);
+
+        string path;
+        static string comstr;
+        static int baudint;
+        string fileName,sd, ss,st, sf = "", sF = "";
+        ToolTip tp = new ToolTip();
+        public Form1()
+        {
+            InitializeComponent();
+            tp.SetToolTip(label4, "Created By Meet Patel../*_ ");
+            // Adding ComPorts intothe comboBox and baudrate
+            Comports();
+            comboBox_comport.SelectedIndex = 0;
+            Baudrate();
+            //comstr = comboBox_comport.Text;
+            //Console.WriteLine(comstr); 
+            //baudint = Convert.ToInt32(ComboBox_baud.Text);
+            // A1ssigning the value od com and baud
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        private void FileOption()
+        {
+            //throw new NotImplementedException();
+            sd = DateTime.Now.ToString("dd/MM/yy");
+            st = DateTime.Now.ToString("hh:mm:ss:tt");
+            string[] dateF = sd.Split('/');
+            string[] timef = st.Split(':');
+            int i, j;
+            for (i = 0, j = 0; i < timef.Length; i++, j++)
+            {
+                if (j < dateF.Length)
+                {
+                    dateF[i] = dateF[i].Trim();
+                    sF = sF + dateF[i]+" ";
+                }
+                timef[j] = timef[j].Trim();
+                sf = sf + timef[i]+" ";
+
+            }
+
+            string folderName = @path + "\\"+sF;
+            string a = "\\";
+            string b = ".txt";
+            fileName = @folderName + a +  sf + b;
+            sF = "";
+            sf = "";
+           // Console.WriteLine(folderName);
+            //Console.WriteLine(fileName);
+            if (!System.IO.Directory.Exists(folderName))
+            {
+                System.IO.Directory.CreateDirectory(folderName);
+                //Console.Write("No");
+            }
+           
+
+            if (!System.IO.File.Exists(fileName))
+            {
+                using (var myfile = System.IO.File.Create(fileName))
+                {
+                    myfile.Close();
+                    
+                }
+
+            }
+           /* else if (File.Exists(fileName))
+            {
+                TextWriter tw = new StreamWriter(fileName, true);
+                tw.WriteLine("Meet Patel");
+                tw.Close();
+            }*/
+        
+
+    }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.DataViewer.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.DataViewer.AppendText(text);
+            }
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            try
+            {
+
+                SerialPort spl = (SerialPort)sender;
+               Console.WriteLine(spl.ReadLine() + "\n");
+                 tw = new StreamWriter(fileName, true);
+                tw.WriteLine(spl.ReadLine()+"\n");
+                tw.Close();
+                //File.Create(fileName).Close();
+                ss = spl.ReadLine() + "\n";
+                SetText(ss);
+               
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Baudrate()
+        {
+            //throw new NotImplementedException();
+            
+            ComboBox_baud.Items.Add(9600);
+            ComboBox_baud.Items.Add(19200);
+            ComboBox_baud.SelectedIndex = 1;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            Console.Beep();
+            try {
+                tw.Close();
+                //System.IO.File.Create(fileName).Close();
+                File.SetAttributes(fileName, FileAttributes.ReadOnly);
+                serialPort1.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+
+        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
+        {
+
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+           
+            folderDlg.ShowNewFolderButton = true;
+            folderDlg.Description = "Select Folder to save the Text File Generated By the Programs";
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxFolderPath.Text = folderDlg.SelectedPath;
+                path = folderDlg.SelectedPath.ToString();
+            }
+        }
+
+        private void textBoxFolderPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Comports()
+        {
+            //throw new NotImplementedException();
+            
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                try {
+                    comboBox_comport.Items.Add(s);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Com port is not added");
+                }
+                
+            }
+            //comboBox_comport.SelectedIndex = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comstr = comboBox_comport.Text;
+            //serialPort1.PortName = comstr;
+        }
+
+        private void ComboBox_baud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            baudint = Convert.ToInt32(ComboBox_baud.Text);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Console.Beep();
+            serialPort1.PortName = comstr;
+            serialPort1.BaudRate = baudint;
+            FileOption();
+            try
+            {
+                // opening the serial port
+                if (!serialPort1.IsOpen)
+                    serialPort1.Open();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+           
+
+        }
+    }
+}
